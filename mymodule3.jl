a53b3105788b6fe729e763c19a14c10b16025e8a
Ivo Balbaert <ivo.balbaert@telenet.be>
2016-09-12 12:20:51 +0200

    "v 0.9"

---
+++ mymodule3.jl
@@ -0,0 +1,8 @@
+module MyModule3
+       
+    export bar, n
+    n = 42
+    bar(x) = 2x
+    foo(x) = 5x^5 - 4
+
+end
---
+++ use_iris_tools.jl
@@ -0,0 +1,17 @@
+include("iris_tools.jl") # IrisTools
+
+using IrisTools # 
+
+# fname # ERROR: UndefVarError: fname not defined
+println(species) # ASCIIString["I. setosa","I. versicolor","I. virginica"]
+
+import IrisTools.fname
+println(fname) # iris.txt
+
+iris1 = Iris(5.1, 3.5, 1.4, 0.2, "I. setosa")
+println(to_string(iris1))
+# This I. setosa has measurements: 
+#     Sepal length: 5.1
+#     Sepal width: 3.5
+#     Petal length: 1.4
+#     Petal width: 0.2
